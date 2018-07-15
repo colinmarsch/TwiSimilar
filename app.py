fc14ec6cf5846b39ad468609b93d49d797a1d1a8
Colin Marsch <colinmarsch@gmail.com>
2018-07-15 00:09:36 -0400

    Fill users_list with users you follow and users they follow

--- app.py
+++ app.py
@@ -9,4 +9,19 @@ api = tweepy.API(auth)
 
 @app.route('/')
 def hello_world():
+    users_list = []
+    current_user = api.me()
+
+    friends_ids = api.friends_ids(screen_name = current_user.screen_name, cursor = -1)
+    for auth_friends_id in friends_ids[0]:
+        print(auth_friends_id)
+        friend = api.get_user(id = auth_friends_id)
+        users_list.append(friend)
+        mutuals = api.friends_ids(screen_name = friend.screen_name, cursor = -1)
+        for id in mutuals[0]:
+            print(id)
+            mutual = api.get_user(id = id)
+            users_list.append(mutual)
+
+    print(users_list)
     return 'Hello, World!'
