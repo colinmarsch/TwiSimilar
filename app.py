f91ffe87daf6f53433eb8052b48e1a1be4a60344
Colin Marsch <colinmarsch@gmail.com>
2018-07-18 21:52:12 -0400

    Added code to create csv files for all the tweets necessary, changed the format of the files

--- app.py
+++ app.py
@@ -13,6 +13,31 @@ api = tweepy.API(auth)
 users_list = []
 current_user = api.get_user(screen_name = 'colinmarsch')
 
+# Get all the tweets of the account that is specified
+print('Getting all the tweets of the specified user.')
+with open('tweets.csv', 'wb') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    writer.writerow(['Tweet', 'User'])
+    for i in range(1, 51):
+        print("Page: " + str(i) + "/" + str(50))
+        try:
+            tweets = api.user_timeline(screen_name = current_user.screen_name, count = 1000, page = i)
+        except tweepy.error.TweepError:
+            print("Couldn't access the tweets of the currently selected account.")
+            break
+        if len(tweets) == 0:
+            break
+        current_tweet_list = []
+        for tweet in tweets:
+            text_val = ''.join([i if (ord(i) < 128 and i not in [',', '\n']) else ' ' for i in tweet.text])
+            text_val = re.sub('[^a-zA-Z]', ' ', text_val).lower().split()
+            ps = PorterStemmer()
+            text_val = [ps.stem(word) for word in text_val if not word in set(stopwords.words('english'))]
+            text_val = ' '.join(text_val)
+            writer.writerow([text_val, current_user.screen_name])
+
+# Get all the users that the account follows
+print('Getting all the users that the account follows')
 friends_ids = api.friends_ids(screen_name = current_user.screen_name, cursor = -1)
 counter = 0
 for auth_friends_id in friends_ids[0]:
@@ -26,10 +51,12 @@ for auth_friends_id in friends_ids[0]:
     #     mutual = api.get_user(id = id)
     #     users_list.append(mutual)
 
+# Get all the tweets of the users that the account follows
+print('Getting all the tweets of the users that the account follows.')
 counter = 0
 with open('users.csv', 'wb') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
-    writer.writerow(['UserID'])
+    writer.writerow(['Tweet', 'User'])
     for user in users_list:
         counter = counter + 1
         print("Writing CSV: " + str(counter) + "/" + str(len(users_list)))
@@ -50,5 +77,4 @@ with open('users.csv', 'wb') as csvfile:
                 ps = PorterStemmer()
                 text_val = [ps.stem(word) for word in text_val if not word in set(stopwords.words('english'))]
                 text_val = ' '.join(text_val)
-                tweet_text_list.append(text_val)
-        writer.writerow([user.screen_name] + tweet_text_list)
+                writer.writerow([text_val, user.screen_name])
