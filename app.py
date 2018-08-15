9ab13dc9019c08b1de39d73f9f77e7f4438bca8b
Colin Marsch <colinmarsch@gmail.com>
2018-08-14 20:53:18 -0400

    Found optimal parameters for LinearSVC, the best model, through grid search

--- app.py
+++ app.py
@@ -16,33 +16,51 @@ y = dataset.iloc[:, 1].values
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.15, random_state = 0)
 
-## Fitting Naive Bayes to the Training set
+## Fitting Naive Bayes to the Training set ~74% mean cross validation score
 #from sklearn.naive_bayes import MultinomialNB
 #classifier = MultinomialNB()
 #classifier.fit(X_train, y_train)
 
-# Fitting LinearSVC to the Training set ~75%
+# Fitting LinearSVC to the Training set ~76.8% mean cross validation score
 from sklearn.svm import LinearSVC
-classifier = LinearSVC()
+classifier = LinearSVC(C = 0.5)
 classifier.fit(X_train, y_train)
 
-## Fitting Logistic Regression to the Training Set
+## Fitting Logistic Regression to the Training Set takes very long time look into this
 #from sklearn.linear_model import LogisticRegression
-#classifier = LogisticRegression(multi_class = 'ovr', solver = 'sag')
+#classifier = LogisticRegression(multi_class = 'multinomial', solver = 'sag')
 #classifier.fit(X_train, y_train)
 
-## Fitting Random Forest to the Training Set
+## Fitting Random Forest to the Training Set Low accuracy
 #from sklearn.ensemble import RandomForestClassifier
-#classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy')
+#classifier = RandomForestClassifier(n_estimators = 200, max_depth = 3)
 #classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
+# Applying k-Fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+accuracies.mean()
+accuracies.std()
+
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 print('Overall Accuracy: ' + str(np.trace(cm) / np.sum(cm)))
+
+# Applying Grid Search to find the best model and the best parameters
+from sklearn.model_selection import GridSearchCV
+parameters = [{'C': [0.25, 0.5, 1, 10], 'dual': [True, False], 'intercept_scaling': [1, 10]}]
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameters,
+                           scoring = 'accuracy',
+                           cv = 10,
+                           verbose = 1000)
+grid_search = grid_search.fit(X_train, y_train)
+best_accuracy = grid_search.best_score_
+best_parameters = grid_search.best_params_
 
 from sklearn.metrics import classification_report
 print(classification_report(y_test, y_pred))
