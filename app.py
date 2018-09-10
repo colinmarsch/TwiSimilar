de9faabec5d74727b857a75d111d96efb9860a53
Colin Marsch <colinmarsch@gmail.com>
2018-09-09 23:41:23 -0400

    Changed status code of response and version of sklearn.

--- app.py
+++ app.py
@@ -56,7 +56,6 @@ def predict_similar():
             ps = PorterStemmer()
             tweet_text = [ps.stem(word) for word in tweet_text if not word in set(stopwords.words('english'))]
             tweet_text = ' '.join(tweet_text)
-            print(tweet_text)
             tweet_text_list.append(tweet_text)
 
     model_pkl = open('tweet_classifier.pkl', 'rb')
@@ -67,4 +66,4 @@ def predict_similar():
     X = vectorizer.transform(tweet_text_list)
     predictions = classifier.predict(X)
 
-    return Counter(predictions).most_common(1)[0]
+    return Counter(predictions).most_common(1)[0], 200
